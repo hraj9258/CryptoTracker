e4240d39418578104f7b31591ae6fc8ea8170593
Himanshu Raj <himanshurajkush@gmail.com>
2024-11-11 19:38:05 +0530

    Upstreamed with https://github.com/philipplackner/CryptoTracker.git

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUI.kt
@@ -0,0 +1,50 @@
+package com.plcoding.cryptotracker.crypto.presentation.models
+
+import android.icu.text.NumberFormat
+import androidx.annotation.DrawableRes
+import com.plcoding.cryptotracker.core.presentation.util.getDrawableIdForCoin
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.crypto.presentation.coin_detail.DataPoint
+import java.util.Locale
+
+data class CoinUI(
+    val id: String,
+    val rank: Int,
+    val name: String,
+    val symbol: String,
+    val marketCapUsd: DisplayableNumber,
+    val priceUsd: DisplayableNumber,
+    val changePercent24Hrs: DisplayableNumber,
+    val coinPriceHistory: List<DataPoint> = emptyList(),
+    @DrawableRes val iconRes: Int
+)
+
+data class DisplayableNumber(
+    val value: Double,
+    val formatted: String
+)
+
+fun Coin.toCoinUi(): CoinUI {
+    return CoinUI(
+        id = id,
+        name = name,
+        symbol = symbol,
+        rank = rank,
+        priceUsd = priceUsd.toDisplayableNumber(),
+        marketCapUsd = marketCapUsd.toDisplayableNumber(),
+        changePercent24Hrs = changePercent24Hr.toDisplayableNumber(),
+        iconRes = getDrawableIdForCoin(symbol)
+    )
+}
+
+fun Double.toDisplayableNumber(): DisplayableNumber {
+    val formatter = NumberFormat.getNumberInstance(Locale.getDefault()).apply {
+        minimumFractionDigits = 2
+        maximumFractionDigits = 2
+    }
+
+    return DisplayableNumber(
+        value = this,
+        formatted = formatter.format(this)
+    )
+}
